73542449bf4aefefe13e8aaa893e96f00cdd0d21
Esteban Piazza <estebanpiazza51@gmail.com>
2022-01-10 18:55:20 -0300

    clase 4

---
+++ Clase 4/variables.js
@@ -0,0 +1,16 @@
+let nombre = "Esteban" // string
+let edad = 28 // numerica
+let edadS = "Esteban" // string
+let booleano = true 
+// El signo + es SUMA y CONCATENACIÓN
+console.log(nombre+1) 
+console.log(edad+1)
+console.log(edadS+1)
+console.log(edad-1)
+console.log(edadS-2) //ESTA MAL (segun criterio del profesor)
+
+console.log(typeof(nombre))
+console.log(typeof(String(edad)))
+console.log(typeof(parseInt(edadS))) //https://www.w3schools.com/jsref/jsref_number.asp
+console.log(typeof(Number(booleano)))
+
