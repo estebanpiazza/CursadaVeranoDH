67b2984038cc9251a57d12adb61fdefec054cc7a
Esteban Piazza <estebanpiazza51@gmail.com>
2022-01-10 19:00:17 -0300

    clase 4 dudas

---
+++ Clase 4/ejercicioIMC.js
@@ -0,0 +1,10 @@
+let Nombre =  "Juana Fernandez " 
+let Edad = 26
+let Peso = 55.0
+let Altura = 1.62
+let prepaga = true
+
+console.log ("El IMC de la paciente " + Nombre + " es " +  Peso / (Altura * Altura), "y su eps es " + prepaga)
+console.log(Altura*Altura)
+console.log(Math.pow(Altura, 2))
+console.log(Peso / Math.pow(Altura, Altura))
