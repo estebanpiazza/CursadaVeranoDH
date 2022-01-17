ae0fa1e6be796c3c8bc0527d8077ba8915dc93d7
Esteban Piazza <estebanpiazza51@gmail.com>
2022-01-17 18:50:19 -0300

    "cambios"

---
+++ Clase 7/operadores.js
@@ -0,0 +1,59 @@
+// booleano > Boole
+/* if (a > b ){ // ¿Es a mayor a b? >Si >No (verdadero o falso) 
+//operador Y  > && > doble ampersan // PYTHON: and
+//operador O  > || > doble pia  // PYTHON: or
+} */
+/* if ( a > b && b > c){} */
+//en python seria "if a > b and b > c:"  
+//expresion o valor- operador- expresion o valor
+/* if (a == b){}
+if (a != b){}
+if (vacunado == true){}
+if( edad > 18){}
+if(nombre == "Esteban"){} */
+
+//Casos de thruthy y falsy
+
+if (true)
+if ({}){console.log("Es vedadero")}
+if ([]){console.log("Es vedadero")}
+if (42)
+if ("foo")
+if (new Date())
+if (-42)
+if (3.14)
+if (-3.14)
+if (Infinity)
+if (-Infinity)
+
+
+//Falsy
+/* false
+0
+-0
+0n
+""
+``
+''
+null
+undefined
+NaN
+document.all
+if (false)
+if (null)
+if (undefined)
+if (0)
+if (-0)
+if (0n)
+if (NaN)
+if ("") */
+
+if (0 == null){console.log("aca estamos")}
+
+console.log("Gato" && "Perro")
+console.log("Gato" || "Perro") 
+console.log(null || "Perro") 
+
+
+
+
